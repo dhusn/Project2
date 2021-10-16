8aeccadc0a86ec090f255a92a5b321e1343e3e30
I I <dhusni2@uic.edu>
2021-10-16 12:40:29 -0500

    project

---
+++ Assets/SpawnLamp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLamp : MonoBehaviour
+{
+    public Rigidbody lamp;
+    public Transform spawnLocation;
+    //bool boolType = false;
+    //private float timer = 0f;
+    float nextTime = 0f;
+    float timer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= nextTime)
+        {
+
+
+            Instantiate(lamp, spawnLocation.position, spawnLocation.rotation);
+
+            nextTime += 30f;
+        }
+        timer += 0.1f;
+
+    }
+}
